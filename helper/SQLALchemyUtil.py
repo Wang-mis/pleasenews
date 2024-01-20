cd32eeb0b6d171cbe89c8cfef340fd6238435f69
wjhers <wujh2117@mails.jlu.edu.cn>
2024-01-20 13:23:38 +0800

    update: SQLAL

--- helper/SQLALchemyUtil.py
+++ helper/SQLALchemyUtil.py
@@ -3,6 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String, DECIMAL, Float
 from sqlalchemy.orm import declarative_base
 
+import pandas as pd
+
+
 # engine = create_engine('sqlite:///:memory:', echo=True)
 engine = create_engine('sqlite:///SQLiteTest.db?check_same_thread=False', echo=True)
 
@@ -10,6 +13,9 @@ Base = declarative_base()
 
 class MergeTable(Base):
     __tablename__ = 'merge_table'
+
+    # 系统自带id 可自增
+    AutoId = Column(Integer, primary_key=True)
 
     GlobalEventID = Column(Integer)
     Day = Column(Integer)
@@ -98,6 +104,8 @@ class MergeTable(Base):
     MentionDocTone = Column(Float)
     MentionDocTranslationInfo = Column(String)
     Extras = Column(String)
+
+    # 为了唯一标记一篇新闻文章
     UniqueID = Column(String)
 
 
@@ -116,31 +124,35 @@ class User(Base):
                             self.name, self.fullname, self.password)
 
 
+    
+
+
 if __name__ == '__main__':
     print(User.__table__)
 
     # 创建数据表。一方面通过engine来连接数据库，另一方面根据哪些类继承了Base来决定创建哪些表
     # checkfirst=True，表示创建表前先检查该表是否存在，如同名表已存在则不再创建。其实默认就是True
-    Base.metadata.create_all(engine, checkfirst=True)
+    # Base.metadata.create_all(engine, checkfirst=True)
+
+    # from sqlalchemy.orm import sessionmaker
+    # Session = sessionmaker(bind=engine)
+    # session = Session() # 实例化会话
+
+    # # 对象实例
+    # user = User(name='ed', fullname='Ed Jones', password='edsnickname')
+    # session.add(user)
+
+    # # 一次插入多条记录形式
+    # session.add_all([
+    #     User(name='wendy', fullname='Wendy Williams', password='foobar'),
+    #     User(name='mary', fullname='Mary Contrary', password='xxg527'),
+    #     User(name='fred', fullname='Fred Flinstone', password='blah')
+    # ])
+
+    # # 当前更改只是在session中，需要使用commit确认更改才会写入数据库
+    # session.commit() # 提交数据
 
 
-    from sqlalchemy.orm import sessionmaker
-    Session = sessionmaker(bind=engine)
-    session = Session() # 实例化会话
-
-    # 对象实例
-    user = User(name='ed', fullname='Ed Jones', password='edsnickname')
-    session.add(user)
-
-    # 一次插入多条记录形式
-    session.add_all([
-        User(name='wendy', fullname='Wendy Williams', password='foobar'),
-        User(name='mary', fullname='Mary Contrary', password='xxg527'),
-        User(name='fred', fullname='Fred Flinstone', password='blah')
-    ])
-
-    # 当前更改只是在session中，需要使用commit确认更改才会写入数据库
-    session.commit() # 提交数据
 
 
     pass

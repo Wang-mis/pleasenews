22d8185174dc505edd8f4ec2576406184744ca16
wjhers <wujh2117@mails.jlu.edu.cn>
2024-01-20 11:42:56 +0800

    update: SQLAL

--- helper/SQLALchemyUtil.py
+++ helper/SQLALchemyUtil.py
@@ -1,12 +1,107 @@
 
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DECIMAL, Float
 from sqlalchemy.orm import declarative_base
 
 # engine = create_engine('sqlite:///:memory:', echo=True)
 engine = create_engine('sqlite:///SQLiteTest.db?check_same_thread=False', echo=True)
 
 Base = declarative_base()
+
+class MergeTable(Base):
+    __tablename__ = 'merge_table'
+
+    GlobalEventID = Column(Integer)
+    Day = Column(Integer)
+    MonthYear = Column(Integer)
+    Year = Column(Integer)
+    FractionDate = Column(Float)
+
+    Actor1Code = Column(String)
+    Actor1Name = Column(String)
+    Actor1CountryCode = Column(String)
+    Actor1KnownGroupCode = Column(String)
+    Actor1EthnicCode = Column(String)
+    Actor1Religion1Code = Column(String)
+    Actor1Religion2Code = Column(String)
+    Actor1Type1Code = Column(String)
+    Actor1Type2Code = Column(String)
+    Actor1Type3Code = Column(String)
+
+    Actor2Code = Column(String)
+    Actor2Name = Column(String)
+    Actor2CountryCode = Column(String)
+    Actor2KnownGroupCode = Column(String)
+    Actor2EthnicCode = Column(String)
+    Actor2Religion1Code = Column(String)
+    Actor2Religion2Code = Column(String)
+    Actor2Type1Code = Column(String)
+    Actor2Type2Code = Column(String)
+    Actor2Type3Code = Column(String)
+
+    IsRootEvent = Column(Integer)
+    EventCode = Column(Integer)
+    EventBaseCode = Column(Integer)
+    EventRootCode = Column(Integer)
+    QuadClass = Column(Integer)
+
+    GoldsteinScale = Column(Float)
+
+    NumMentions = Column(Integer)
+    NumSources = Column(Integer)
+    NumArticles = Column(Integer)
+
+    AvgTone = Column(Float)
+
+    Actor1Geo_Type = Column(Integer)
+    Actor1Geo_Fullname = Column(String)
+    Actor1Geo_CountryCode = Column(String)
+    Actor1Geo_ADM1Code = Column(String)
+    Actor1Geo_Lat = Column(String)
+    Actor1Geo_Long = Column(String)
+    Actor1Geo_FeatureID = Column(String)
+
+    Actor2Geo_Type = Column(Integer)
+    Actor2Geo_Fullname = Column(String)
+    Actor2Geo_CountryCode = Column(String)
+    Actor2Geo_ADM1Code = Column(String)
+    Actor2Geo_Lat = Column(String)
+    Actor2Geo_Long = Column(String)
+    Actor2Geo_FeatureID = Column(String)
+
+    ActionGeo_Type = Column(Integer)
+    ActionGeo_Fullname = Column(String)
+    ActionGeo_CountryCode = Column(String)
+    ActionGeo_ADM1Code = Column(String)
+    ActionGeo_Lat = Column(String)
+    ActionGeo_Long = Column(String)
+    ActionGeo_FeatureID = Column(String)
+
+    DATEADDED = Column(Integer)
+    SOURCEURL = Column(String)
+
+    EventTimeDate = Column(Integer)
+    MentionTimeDate = Column(Integer)
+    MentionType = Column(Integer)
+
+    MentionSourceName = Column(String)
+    MentionIdentifier = Column(String)
+
+    SentenceID = Column(Integer)
+    Actor1CharOffset = Column(Integer)
+    Actor2CharOffset = Column(Integer)
+    ActionCharOffset = Column(Integer)
+    InRawText = Column(Integer)
+    Confidence = Column(Integer)
+    MentionDocLen = Column(Integer)
+
+    MentionDocTone = Column(Float)
+    MentionDocTranslationInfo = Column(String)
+    Extras = Column(String)
+    UniqueID = Column(String)
+
+
+
 
 class User(Base):
     __tablename__ = 'users'

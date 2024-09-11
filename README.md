fb778afef82cb01cd3d461e919e3f0189d59a403
Wang-mis <1753143830@qq.com>
2024-09-11 22:47:18 +0800

    为关键词提取模块田é添加词形æ¬还原功能

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# pleasenews
-Get hot news reports from renowned news publishers.
+# PleaseNews
+
+从GDELT数据库中获取数据，进行处理后生成SQLiteTest.db数据库文件。
+
+## downcsv.py
+
+下载GDELT数据库中的数据。包括GDELT1.0中的.exports.csv文件和GDELT2.0中的.mentions.csv文件，分别保存在./export和./mentions文件夹中。
+
+### .exports.csv
+
+记录了一天内报道的所有事件。一条记录代表一个事件，GlobalEventID是他的唯一标识符。一个事件可以由多条新闻分别报道，所有SourceURL字段只记录第一条报道该事件的新闻URL。
+
+### .mentions.csv
+
+记录的某一天的15分钟内的每一条新闻报道和一个事件的对应，一个新闻可以包含多个事件，一个事件也可以被多条新闻报道，所以GlobalEventID和MentionIdentifier均不唯一。
+
+## Processcsv.py
+
+### .merge.csv
+
+ 将同一天的.exports.csv和.mentions.csv按GlobalEventID进行内连接，合并两个文件，并进行排序、去重、添加唯一标识UniqueID等处理，产生.merge.csv文件。
+
+### .media.merge.csv
+
+从.merge.csv中筛选出在MEDIUMLIST列表中的新闻媒体发布的文章，保存在.media.merge.csv中。
+
+##  crawlArticlesForDiffSourceURL.py
+
+从.media.merge.csv中读取same_struct_domain_list列表中的所有媒体发布的文章的网址，爬取新闻。
+
+./txt/DAY/unique_url_about_SOURCE_URL.txt 保存当天某一个媒体发布的所有新闻的网址
+
+./txt/DAY/error_url_SOURCE_URL.txt 保存爬虫爬取失败的新闻网址
+
+./articles/DAY/SOURCE_URL/UNIQUE_ID.txt 保存爬取的新闻标题、作者、日期、内容
+
+> 爬取到的文章的发布日期与数据集中提供的日期可能不同，因为数据集中的日期是事件发生的日期，不是文章发布日期。
+
+## processarticle.py
+
+将crawlArticlesForDiffSourceURL.py爬取到的所有文章合并到一个csv文件中。
+
+./pnews/DAY/SOURCE_URL.csv
+
+./pnews/DAY/MentionSourceNames.csv
+
+## ztest.py
+
+读取./pnews/DAY/MentionSourceNames.csv，获取每篇文章的关键词，保存为Keywords_check.csv
+
--- zlog/ztest.py
+++ zlog/ztest.py
@@ -1,65 +1,39 @@
 import os
-import openai
 import pandas as pd
-import helper
-import time
+import spacy
 from keybert import KeyBERT
 from tqdm import tqdm
-
 from utils import create_date_range, TIME_RANGE
 
-# os.environ['HTTPS_PROXY'] = 'http://127.0.0.1:1080'
-# os.environ["HTTP_PROXY"] = 'http://127.0.0.1:1080'
 os.environ['HTTPS_PROXY'] = 'http://127.0.0.1:7890'
 os.environ["HTTP_PROXY"] = 'http://127.0.0.1:7890'
 
-# SAVE_NEWS = "../pnews/20240131/"
-SAVE_NEWS = "../pnews/20240901/"
+DAY = "20240901"
+SAVE_NEWS = "../pnews/" + DAY + "/"
 MEIDA_CONTENT = SAVE_NEWS + "MentionSourceNames.csv"
 MEDIA_KEYWORD = SAVE_NEWS + "Keywords.csv"
+MEDIA_KEYWORD_CHECK = SAVE_NEWS + "Keywords_check.csv"
 
-OPENAI_API_KEY = "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"  # gpt-4
-openai.api_key = OPENAI_API_KEY
-
-
-def get_openai_completion(prompt, model="gpt-4", temperature=0):
-    """
-    prompt: question
-    model:  gpt-3.5-turbo(ChatGPT) / text-davinci-003 , gpt-4
-    """
-    try:
-        messages = [{"role": "user", "content": prompt}]
-        response = openai.ChatCompletion.create(
-            model=model,
-            messages=messages,
-            # max_tokens=256,
-            n=1,
-            temperature=temperature,  # 模型输出的温度系数，控制输出的随机程度
-        )
-        # 调用 OpenAI 的 ChatCompletion 接口
-        print(response.choices[0].message["content"])
-    except Exception as e:
-        print(e)
-        return "error..."
-
-    return response.choices[0].message["content"]
+SPACY = spacy.load('en_core_web_sm')
 
 
-def gainKeywordByKeyBERT():
-    print(MEIDA_CONTENT)
+def generate_keywords_by_KeyBERT():
     df = pd.read_csv(MEIDA_CONTENT)
     UniqueIDList = df["UniqueID"].to_list()
     ContentList = df["Content"].to_list()
     KeywordList = []
-    print(len(UniqueIDList))
 
     kw_model = KeyBERT()
 
-    for index, content in tqdm(enumerate(ContentList)):
+    process_bar = tqdm(total=len(UniqueIDList), desc=DAY + " 生成关键词中")
+    for index, content in enumerate(ContentList):
         try:
+            doc = SPACY(content)
+            content = " ".join([token.lemma_ for token in doc])
             keywords = kw_model.extract_keywords(content, keyphrase_ngram_range=(1, 1), stop_words='english', top_n=15)
             keywords = "|".join([ele[0] for ele in keywords])
             KeywordList.append(keywords)
+            process_bar.update(1)
         except Exception as e:
             print(e)
             print(index)
@@ -74,106 +48,41 @@ def gainKeywordByKeyBERT():
 
     # 使用字典创建数据框
     df_key = pd.DataFrame(data)
-    df_key.to_csv(SAVE_NEWS + "Keywords.csv", index=False, encoding='utf-8', errors='ignore')
+    df_key.to_csv(MEDIA_KEYWORD, index=False, encoding='utf-8', errors='ignore')
     pass
 
 
-# 为爬取的新闻文章提取关键词
-def gainKeywordByGPT4():
-    print(MEIDA_CONTENT)
-    df = pd.read_csv(MEIDA_CONTENT)
-    UniqueIDList = df["UniqueID"].to_list()
-    ContentList = df["Content"].to_list()
-    KeywordList = []
+def process_keywords():
+    def parse_keywords(x):
+        if x == "error..." or "'gbk' codec can't encode character" in x:
+            return ""
 
-    print(len(UniqueIDList))
+        key_list = [ele.strip() for ele in x.split("|")]
+        key_list1 = []
+        for ele in key_list:
+            key_list1 += [i.strip("\'\"") for i in ele.split(",")]
 
-    for index, content in tqdm(enumerate(ContentList)):
-        time.sleep(2)
-        prompt = "Please understand the following text and extract keywords from it. \
-            The number of keywords should not be less than 3 and should not exceed 15. \
-            The return value should be of type str in Python, separated by a '|' symbol between each keyword. \
-            This passage is: '{}'".format(content)
+        ans = "|".join(key_list1)
+        return ans
 
-        print(index)
-
-        keywords = get_openai_completion(prompt=prompt)
-        KeywordList.append(keywords)
-
-    # print(KeywordList)
-
-    # 创建一个字典，包含列名和相应的数据
-    data = {
-        'UniqueID': UniqueIDList,
-        'Keyword': KeywordList,
-    }
-
-    # 使用字典创建数据框
-    df_key = pd.DataFrame(data)
-    df_key.to_csv(SAVE_NEWS + "Keywords.csv", index=False)
-
-    return
-
-
-# 自定义函数
-def parse_keyword(x):
-    if x == "error..." or "'gbk' codec can't encode character" in x:
-        return ""
-
-    key_list = [ele.strip() for ele in x.split("|")]
-    key_list1 = []
-    for ele in key_list:
-        key_list1 += [i.strip("'\"") for i in ele.split(",")]
-
-    ans = "|".join(key_list1)
-    return ans
-
-
-def processKeyword():
     df = pd.read_csv(MEDIA_KEYWORD).fillna("")
-    df['Keyword'] = df['Keyword'].apply(lambda x: parse_keyword(x))
-    df.to_csv(SAVE_NEWS + "Keywords_check.csv", index=False)
+    df['Keyword'] = df['Keyword'].apply(lambda x: parse_keywords(x))
+    df.to_csv(MEDIA_KEYWORD_CHECK, index=False)
     return
-
-
-def oneDayMediaListFun():
-    # 查看全球媒体 20240121 报道文章数量
-    MERGE_FILE = "../merge/NULL/20240121.merge.csv"
-    df = pd.read_csv(MERGE_FILE)
-    result = df.groupby("MentionSourceName").size().to_dict()
-    media_list_today = helper.sortCustomDict(result)
-
-    helper.Dict2Json("./media_list_today.json", media_list_today)
-    return
-
-
-def otfunc():
-    import pandas as pd
-    # 创建两个数据框
-    df1 = pd.DataFrame({'ID': [1, 2, 3, 3], 'Name': ['Alice', 'Bob', 'Charlie', 'AA']})
-    df2 = pd.DataFrame({'ID': [2, 3, 3, 4], 'Age': [25, 30, 40, 35]})
-    # 内连接两个数据框，基于 'ID' 列进行匹配
-    result_df = pd.merge(df1, df2, on='ID', how='inner')
-    print(result_df)
 
 
 def get_keywords():
-    global SAVE_NEWS, MEIDA_CONTENT, MEDIA_KEYWORD
+    global DAY, SAVE_NEWS, MEIDA_CONTENT, MEDIA_KEYWORD, MEDIA_KEYWORD_CHECK
 
-    # oneDayMediaListFun()
     days = create_date_range(TIME_RANGE)
     for day in days:
-        SAVE_NEWS = "../pnews/" + str(day) + "/"
+        DAY = str(day)
+        SAVE_NEWS = "../pnews/" + DAY + "/"
         MEIDA_CONTENT = SAVE_NEWS + "MentionSourceNames.csv"
         MEDIA_KEYWORD = SAVE_NEWS + "Keywords.csv"
-
-        # gainKeywordByGPT4()
-        gainKeywordByKeyBERT()
-        processKeyword()
-
-        # re = get_openai_completion("hi")
-        # print(re)
-        pass
+        MEDIA_KEYWORD_CHECK = SAVE_NEWS + "Keywords_check.csv"
+        generate_keywords_by_KeyBERT()
+        process_keywords()
 
 
 if __name__ == "__main__":
